18d69ad767b68b41a14b60afc48075a0c96cedab
Mike Smith <m.t.smith@sheffield.ac.uk>
2025-05-07 08:28:23 +0100

    modularising

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+  name = 'seedtrack3d',
+  packages = ['seedtrack3d'],
+  version = '1.0',
+  description = 'Tracks seeds using a camera and a mirror, in 3d.',
+  author = 'Mike Smith',
+  author_email = 'm.t.smith@sheffield.ac.uk',
+  url = 'https://github.com/lionfish0/seedtrack3d',
+  download_url = 'https://github.com/lionfish0/seedtrack3d.git',
+  keywords = ['seeds','3d','calibration'],
+  classifiers = [],
+  install_requires=[],
+)
